e8ea379f099e14fea5ea73c03383280ea158f2ac
Katsuya Endoh <studio@enkatsu.org>
2022-11-20 04:40:46 +0900

    Updates

---
+++ component---src-pages-404-tsx-9b2c5d490cddbaeb1b6a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproject_p5js_jp=self.webpackChunkproject_p5js_jp||[]).push([[218],{2513:function(e,t,n){n.r(t),n.d(t,{Head:function(){return r}});var o=n(7294);const a={color:"#232129",padding:"96px",fontFamily:"-apple-system, Roboto, sans-serif, serif"},s={marginTop:0,marginBottom:64,maxWidth:320};t.default=()=>o.createElement("main",{style:a},o.createElement("h1",{style:s},"Page not found"));const r=()=>o.createElement("title",null,"Not found")}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-9b2c5d490cddbaeb1b6a.js.map
